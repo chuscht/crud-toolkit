38cc49010c65914c11e705a50bc2bdc603565a14
Cristian Hernan Huscht <cristianhuscht21@gmail.com>
2023-03-23 09:42:40 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
-
+import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { useSelector } from "react-redux";
+import TasksForm from "./components/TasksForm";
+import TasksList from "./components/TasksList";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="bg-zinc-900 h-screen text-white">
+      <div className="flex items-center justify-center h-full">
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<TasksList />} />
+            <Route path="/create-task" element={<TasksForm />} />
+            <Route path="/edit-task/:id" element={<TasksForm />} />
+          </Routes>
+        </BrowserRouter>
+      </div>
     </div>
   );
 }
 
 export default App;
+
+//50 min del video ya logre capturar el id con el boton delete //
